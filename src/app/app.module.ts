0818a6cbe59c58e605f7660b5427de79232ed674
owoturotimi@live.co.uk <owoturotimi@live.co.uk>
2018-06-19 21:51:09 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {RouterModule, Routes} from '@angular/router';
+import {MatDividerModule} from '@angular/material/divider';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+const routes: Routes = [
+  {path: '', component: AppComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    RouterModule.forRoot(
+      routes,
+    ),
+    MatDividerModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
